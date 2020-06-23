99a4b4c3021d4a6267079395f5b274a9484a2997
Zh00c3q <maciej.a.zak@gmail.com>
2020-06-23 16:14:24 +0200

    add hexed that fill with honey when bee touches them (TriggerOnEnter2D)

---
+++ Assets/Scripts/GenerateHexes.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateHexes : MonoBehaviour
+{
+    public GameObject HexPrefab;
+
+    int height = 15;
+    int width = 5;
+    float xOffset = 1.74f;
+    float zOffset = 0.5f;
+    Vector2 Map_offset =new Vector2(-4f,-3f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                float xPos = x * xOffset;
+
+                if (y % 2 == 1)
+                {
+                    xPos += xOffset / 2f;
+                }
+
+                GameObject Tile_go = (GameObject)Instantiate(HexPrefab, Map_offset + new Vector2(xPos, y * zOffset), Quaternion.identity);
+                Tile_go.transform.SetParent(this.transform);
+                Tile_go.name = "Hex_" + x + "_" + y;
+
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/honeyHex.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class honeyHex : MonoBehaviour
+{
+
+    [SerializeField] public int status;
+    SpriteRenderer MySprite;
+    Animator MyAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MySprite = GetComponentInChildren<SpriteRenderer>();
+        MyAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FillHexWithHoney();
+    }
+
+    void FillHexWithHoney()
+    {
+        {
+            MyAnimator.SetInteger("status", status);
+        }
+
+    
+    }
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        GameObject otherObject = otherCollider.gameObject;
+         if (otherObject.GetComponent<Bee>())
+         {
+             status += 1;
+         }
+        
+    }
+}
