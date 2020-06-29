11123623bcb1a8fb3391feb4a3c70d11bd90a21c
AnnPla <anna.platek2222@gmail.com>
2020-06-29 22:02:50 +0200

    add map scene and script LevelChoice that allows player to move start working on the method OnTriggerEnter2D that allows to load chosen level

---
+++ Assets/Scripts/LevelsChoice.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityStandardAssets.CrossPlatformInput;
+
+
+public class LevelsChoice : MonoBehaviour
+{
+    [SerializeField] float playerSpeed = 2f;
+    public LevelsChoice player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<LevelsChoice>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * playerSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        GameObject otherObject = otherCollider.gameObject;
+        Debug.Log(otherObject.name);
+        //TODO: zrobić level loader z wyborem poziomów
+        //TODO: funkcja ze znajdowaniem nazwy obiektu
+        //TODO: funkcja która ładuje poziom w zależności od tego, na jakim obiekcie jest gracz
+    }
+}
