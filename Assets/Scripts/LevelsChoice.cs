026d6666a7dd3c259ecb916530580938ae5f720d
AnnPla <anna.platek2222@gmail.com>
2020-07-02 20:47:24 +0200

    modify LevelChoice script so that appropiate level will load after touching level icon by player and pressing enter

--- Assets/Scripts/LevelsChoice.cs
+++ Assets/Scripts/LevelsChoice.cs
@@ -1,23 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 //using UnityStandardAssets.CrossPlatformInput;
 
 
 public class LevelsChoice : MonoBehaviour
 {
     [SerializeField] float playerSpeed = 2f;
-    public LevelsChoice player;
+    [SerializeField] float loadLevelDelay = 3f;
+    public string levelName;
+    bool canMove = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        player = FindObjectOfType<LevelsChoice>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
+        if (canMove == true)
+        {
+            Move();
+        }
+        SelectLevel();
+        Debug.Log("Level name: " + levelName);
+
+    }
+
+    public void SelectLevel()
+    {
+        if (levelName != null && Input.GetKeyDown("return")) //TODO: nie tylko enter, ale przycisk na padzie
+        {
+            StartCoroutine(LoadChosenLevel());
+            
+        }
     }
 
     public void Move()
@@ -29,9 +47,26 @@ public class LevelsChoice : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D otherCollider)
     {
         GameObject otherObject = otherCollider.gameObject;
+        levelName = otherObject.name;
         Debug.Log(otherObject.name);
-        //TODO: zrobić level loader z wyborem poziomów
-        //TODO: funkcja ze znajdowaniem nazwy obiektu
-        //TODO: funkcja która ładuje poziom w zależności od tego, na jakim obiekcie jest gracz
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        levelName = null;
+    }
+
+    IEnumerator LoadChosenLevel()
+    {
+        canMove = false;
+        yield return new WaitForSecondsRealtime(loadLevelDelay);
+        if (levelName != null)
+        {
+            FindObjectOfType<LevelLoader>().LoadLevel(levelName);
+        }
+        else
+        {
+            Debug.Log("Invalid level name");
+        }
     }
 }
