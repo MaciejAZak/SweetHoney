e70e9443c2f578a01ad1f468919cce61a3c75e3c
Zh00c3q <maciej.a.zak@gmail.com>
2020-07-16 15:04:20 +0200

    add a hex_menu prefab that snaps to grid in edit mode

---
+++ Assets/HexPlacement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class HexPlacement : MonoBehaviour
+{
+    [SerializeField] int X = 0;
+    [SerializeField] int Y = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 snapPos;
+        if (X % 2 == 0)
+        {
+            snapPos.x = X * 1.74f;
+            snapPos.y = Y;
+        }
+        else
+        {
+            snapPos.x = X * 0.87f;
+            snapPos.y = Y + 0.5f;
+        }
+
+        transform.position = new Vector3(snapPos.x, snapPos.y, 0f);
+        this.name = "Hex_" + X + "_" + Y;
+    }
+}
